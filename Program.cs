eb972227f1e6f8c66f0cec0a06e0ab9e964a4fe2
andrey.nascimento4@etec.sp.gov.br <e107ti@etec.sp.gov.br>
2023-05-19 20:07:52 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿int numero;
+
+Console.WriteLine("DIGITE UM NÚMERO PARA CÁLCUOLO DA TABUADA:");
+numero = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine($"numero digitado :{numero}");
+int contador = 1;
+
+while (contador <= 10)
+{
+    int resultado = numero * contador;
+    Console.WriteLine($"{numero} x {contador} = {resultado}");
+    Thread.Sleep(200);
+    contador = contador + 1;
+}
+
+Console.WriteLine("FIM");
